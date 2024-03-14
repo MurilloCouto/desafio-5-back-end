b6380fdb3dc0976798cfde4a9df1602b685d813f
Murillo Alves Couto <murilloacouto@outlook.com>
2024-03-14 20:03:30 -0300

    corrigindo string de conexão com o mongodb atlas

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const MONGODB_URI =
   process.env.MONGODB_URI ||
-  "mongodb+srv://murilloacouto:PzJLzPihVvt3xnbG@cluster1.idmwgqi.mongodb.net";
+  "mongodb+srv://murilloacouto:murillo97@cluster1.idmwgqi.mongodb.net";
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
