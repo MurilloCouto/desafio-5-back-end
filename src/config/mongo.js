c228d14732a274e78757e3dae38153dfdcbd04c7
Murillo Alves Couto <murilloacouto@outlook.com>
2024-03-14 08:36:51 -0300

    corrigindo conexão com o banco de dados

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://murilloacouto:PzJLzPihVvt3xnbG@cluster1.idmwgqi.mongodb.net";
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
