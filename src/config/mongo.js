da8753e145b4d1f2780a2d6ca36a93e16ce32c43
Murillo Alves Couto <murilloacouto@outlook.com>
2024-03-14 21:02:39 -0300

    corrigindo string de conexao

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -4,9 +4,6 @@ const MONGODB_URI =
   process.env.MONGODB_URI ||
   "mongodb+srv://murilloacouto:murillo97@cluster1.idmwgqi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 export default mongoose;
