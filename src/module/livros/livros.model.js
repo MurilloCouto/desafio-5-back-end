b8484d72828953b5b8369b94d0a8b9134c4812b7
Murillo Alves Couto <murilloacouto@outlook.com>
2024-03-18 12:14:40 -0300

    corrigindo rotas do arquivo server.js e modelo do banco de dados

--- src/module/livros/livros.model.js
+++ src/module/livros/livros.model.js
@@ -3,12 +3,11 @@ const { Schema } = mongoose;
 
 const newsSchema = new Schema(
   {
-    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-    id: { type: Number, required: true },
-    titulo: { type: String, required: true },
-    num_paginas: { type: Number, required: true },
-    isbn: { type: String, required: true },
-    editora: { type: String, required: true },
+    id: Number,
+    titulo: String,
+    num_paginas: Number,
+    isbn: String,
+    editora: String,
   },
   {
     timestamps: true,
--- src/server.js
+++ src/server.js
@@ -9,90 +9,37 @@ app.options("*", cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/livros", async (_, res) => {
+app.get("/livros", async (req, res) => {
   const livros = await LivrosModel.find({});
   return res.status(200).json(livros);
 });
 
 app.post("/livros/cadastro", async (req, res) => {
-  const { id, titulo, num_paginas, isbn, editora } = req.body;
-
-  try {
-    const response = await LivrosModel.create({
-      id: id,
-      titulo: titulo,
-      num_paginas: num_paginas,
-      isbn: isbn,
-      editora: editora,
-    });
-
-    return res.status(200).json({
-      data: response,
-    });
-  } catch (error) {
-    console.error("Erro ao cadastrar livro:", error);
-    return res.status(500).json({ mensagem: "Erro ao cadastrar livro." });
-  }
+  const response = await LivrosModel.create({
+    id: req.body.id,
+    titulo: req.body.titulo,
+    num_paginas: req.body.num_paginas,
+    isbn: req.body.isbn,
+    editora: req.body.editora,
+  });
+  return res.status(200).json({
+    data: response,
+  });
 });
 
 app.get("/livros/edicao/:id", async (req, res) => {
-  try {
-    const livroId = req.params.id;
-    const livro = await LivrosModel.findOne({ id: livroId });
-
-    if (!livro) {
-      return res.status(404).json({ mensagem: "Livro não encontrado." });
-    }
-
-    return res.status(200).json(livro);
-  } catch (error) {
-    console.error("Erro durante a busca do livro:", error);
-    return res.status(500).json({ mensagem: "Erro durante a busca do livro." });
-  }
+  const livro = await LivrosModel.findOne({ id: req.params.id });
+  return res.status(200).json(livro);
 });
 
 app.put("/livros/edicao/:id", async (req, res) => {
-  try {
-    const livroId = req.params.id;
-
-    const updatedLivro = await LivrosModel.findOneAndUpdate(
-      { id: livroId },
-      req.body,
-      { new: true }
-    );
-
-    if (!updatedLivro) {
-      return res.status(404).json({ mensagem: "Livro não encontrado." });
-    }
-
-    return res.status(200).json({ mensagem: "Livro atualizado com sucesso!" });
-  } catch (error) {
-    console.error("Erro durante a edição do livro:", error);
-    return res
-      .status(500)
-      .json({ mensagem: "Erro durante a edição do livro." });
-  }
+  const livro = await LivrosModel.updateOne({ id: req.params.id }, req.body);
+  return res.status(200).json(livro);
 });
 
 app.delete("/livros/:id", async (req, res) => {
-  try {
-    const livroId = req.params.id;
-
-    const deletedLivro = await LivrosModel.findOneAndDelete({
-      id: livroId,
-    });
-
-    if (!deletedLivro) {
-      return res.status(404).json({ mensagem: "Livro não encontrado." });
-    }
-
-    return res.status(200).json({ mensagem: "Livro removido com sucesso!" });
-  } catch (error) {
-    console.error("Erro durante a remoção do livro:", error);
-    return res
-      .status(500)
-      .json({ mensagem: "Erro durante a remoção do livro." });
-  }
+  const livro = await LivrosModel.deleteOne({ id: req.params.id });
+  return res.status(200).json(livro);
 });
 
 app.listen(PORT, () => {
